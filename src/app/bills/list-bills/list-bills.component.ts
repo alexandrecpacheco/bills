a954f222275ac410ec2eb344f5ae887dbee46fa5
Alexandre Cardoso Pacheco <esx37@localiza.com>
2022-06-04 14:17:55 -0300

    Scss changes to fix the layout

--- src/app/bills/list-bills/list-bills.component.ts
+++ src/app/bills/list-bills/list-bills.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { Router } from '@angular/router';
+import { DetailBillsComponent } from '../detail-bills/detail-bills.component';
 
 @Component({
   selector: 'app-list-bills',
   templateUrl: './list-bills.component.html',
   styleUrls: ['./list-bills.component.scss']
 })
+
 export class ListBillsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(DetailBillsComponent) private detailComponent!: DetailBillsComponent;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  debitosMensais($event: any){
-    if ($event.index === 0)
-      window.location.reload();
+  onTabChanged(event: MatTabChangeEvent) {
+    if(event.index == 0)
+        this.detailComponent.getPaymentBills();
   }
+  
+  // reloadCurrentPage(){
+  //   let currentUrl = this.router.url;
+  //   this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+  //       this.router.navigate([currentUrl]);
+  //   });
+  // }
  }
