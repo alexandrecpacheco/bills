1d33afd5f98de5f880393e59f1c0988af80ba8ec
Alexandre C Pacheco <alexandre.cpacheco@gmail.com>
2024-01-31 18:00:03 -0300

    Code fix of all stylesheets to overflow the page

--- src/app/shared/create-button/create-button.component.ts
+++ src/app/shared/create-button/create-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-button',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   createItem(){
-
+    this.router.navigate(['/bills']);
   }
 }
